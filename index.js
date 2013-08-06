7c6898ee0945222c32cc6c7598e3b2e37d4ffb07
Brian LeRoux <brian@westcoastlogic.com>
2013-08-06 16:41:43 -0700

    spaces drive me nuts

--- index.js
+++ index.js
@@ -14,22 +14,22 @@ function caesar(shift, input) {
 	
     var output = '', len = input.length
 
-	for (var i = 0; i < len; i++) {
+    for (var i = 0; i < len; i++) {
 
-		var c = input.charCodeAt(i)
+        var c = input.charCodeAt(i)
 
-		if (c >= 65 && c <=  90) { 
+        if (c >= 65 && c <=  90) { 
             // upcase
             output += String.fromCharCode((c - 65 + shift) % 26 + 65)
         }  
-		else if (c >= 97 && c <= 122) {  
+        else if (c >= 97 && c <= 122) {  
             // downcase
             output += String.fromCharCode((c - 97 + shift) % 26 + 97) 
         }
-		else {
+        else {
             // copy thru
             output += input.charAt(i)
         }
-	}
-	return output
+    }
+    return output
 }
